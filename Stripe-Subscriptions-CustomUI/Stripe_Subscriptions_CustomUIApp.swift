2f5a4668e8efbe941c033e81265f1b566073efb9
Dinesh Vijaykumar <dinesh.vijaykumar@conjure.co.uk>
2022-03-14 14:54:39 +0000

    Initial Commit

---
+++ Stripe-Subscriptions-CustomUI/Stripe_Subscriptions_CustomUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stripe_Subscriptions_CustomUIApp.swift
+//  Stripe-Subscriptions-CustomUI
+//
+//  Created by Dinesh Vijaykumar on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Stripe_Subscriptions_CustomUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
