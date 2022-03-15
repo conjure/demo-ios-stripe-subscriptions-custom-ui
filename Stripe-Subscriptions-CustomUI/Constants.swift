e870cb95c89c154f29509ddf5c8970476f4cfd44
Dinesh Vijaykumar <dinesh.vijaykumar@conjure.co.uk>
2022-03-15 12:09:08 +0000

    Add server code to repo

--- Stripe-Subscriptions-CustomUI/Constants.swift
+++ Stripe-Subscriptions-CustomUI/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 import Foundation
 
 struct Constants {
-    static let stripePublishableKey = "ENTER_STRIPE_PUBLISHABLE_API_KEY"
-    static let backendUrl = "ENTER_BACKEND_URL"
-    static let priceID = "ENTER_STRIPE_PRICE_ID"
+    static let stripePublishableKey = "pk_test_51JAtyZLkteLFS4C4Yeh7ugIxOEv94U8xcf4pfPfhQGf5ZSZ6Mraouc0POekA2LgYLV8QqTWebnIc3zf5SXR9qCpT003M9EWk12"
+    static let backendUrl = "https://petalite-slash-boa.glitch.me"
+    static let priceID = "price_1JwrfXLkteLFS4C4b8hmgmYc"
 }
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+const stripe = require("stripe")(process.env.secret_key); // https://stripe.com/docs/keys#obtain-api-keys
+
+app.use(express.static("."));
+app.use(express.json());
+
+// An endpoint for creating a new Stripe customer
+app.post("/subscribe", async (req, res) => {
+  // Create or retrieve the Stripe Customer object associated with your user.
+  let customer = await stripe.customers.create({
+    email: req.body.email,
+  });
+
+  // Attach payment method to the customer
+  const paymentMethod = await stripe.paymentMethods.attach(
+    req.body.paymentMethodId,
+    { customer: customer.id }
+  );
+
+  // Create subscription for customer
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ price: req.body.priceID }],
+    expand: ["latest_invoice.payment_intent"],
+    default_payment_method: paymentMethod.id,
+  });
+
+  // Send the client secret of payment intent to the client
+  res.send({
+    paymentIntent: subscription.latest_invoice.payment_intent.client_secret,
+    customer: customer.id,
+  });
+});
